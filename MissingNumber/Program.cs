f6b84ba5df5dd88d8cd99d11f556ff689e66a862
Samvy19 <sambhavi.19@outlook.com>
2025-06-19 17:36:31 +1000

    Update Program.cs

--- MissingNumber/Program.cs
+++ MissingNumber/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main()
     {
-        int[] input = { 2, 6, 1, 3, 4, 5, 8, 0}; // Example: missing number is 2
+        int[] input = { 2, 6, 1, 3, 4, 5, 8, 0}; // Example: missing number is 7
 
         IValidationService validation = new ValidationService();
         ICalculationService calculation = new CalculationService(validation);
